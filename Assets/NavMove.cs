e0684f46920737b9a2196ca75e209ad3210daee1
wow <wow@daum-naver.com>
2017-01-07 04:35:15 +0900

    nav mesh Test

---
+++ Assets/NavMove.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class NavMove : MonoBehaviour {
+
+    public GameObject Target;
+
+    void Start()
+    {
+        //NavMesh.SetLayerCost(3, 2);
+        int layer = NavMesh.GetNavMeshLayerFromName("path1");
+        NavMesh.SetLayerCost(layer, 2);
+
+        GetComponent<NavMeshAgent>().SetDestination(Target.transform.position);
+    }
+}
