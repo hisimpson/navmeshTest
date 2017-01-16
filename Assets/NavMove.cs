81e59f51e1f49e5cbf60ef61dba355612f4bea73
wow <wow@daum-naver.com>
2017-01-17 06:58:19 +0900

    Navmesh - Off Mesh Link 이용

--- Assets/NavMove.cs
+++ Assets/NavMove.cs
@@ -3,13 +3,24 @@
 public class NavMove : MonoBehaviour {
 
     public GameObject Target;
+    NavMeshAgent agent;
 
     void Start()
     {
-        //NavMesh.SetLayerCost(3, 2);
-        int layer = NavMesh.GetNavMeshLayerFromName("path1");
-        NavMesh.SetLayerCost(layer, 2);
+        agent = GetComponent<NavMeshAgent>();
+
+        //NavMesh.SetAreaCost(3, 2);
+        int layer = NavMesh.GetAreaFromName("path1");
+        NavMesh.SetAreaCost(layer, 2);
 
         GetComponent<NavMeshAgent>().SetDestination(Target.transform.position);
+    }
+
+    void Update()
+    {
+        if (agent.isOnOffMeshLink)
+        {
+            agent.CompleteOffMeshLink();
+        }
     }
 }
